cc7bea51454d19fa0493fa9fa81e30647959a6c4
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2026-03-17 10:23:32 +0000

    fixed the way the account id is fetch as it was mixing multiple fields.

--- src/jira/user.rs
+++ src/jira/user.rs
@@ -14,25 +14,30 @@ impl DefaultCurrentUserService {
     pub fn new(jira_api: Arc<dyn JiraApi>) -> Self {
         Self { jira_api }
     }
-}
 
-impl CurrentUserService for DefaultCurrentUserService {
-    fn fetch_current_account_id(&self) -> Option<String> {
-        let jql = "assignee+in+(currentUser())+OR+reporter+in+(currentUser())+OR+creator+in+(currentUser())";
+    fn get_user_id_by(&self, field: &str) -> Option<String> {
+        let f = field.to_string();
+        let jql = f.to_string() + "+in+(currentUser())";
         let response = self
             .jira_api
             .get_v3(&format!("search?maxResults=1&jql={jql}"))
             .ok()?;
 
         let fields = &response["issues"][0]["fields"];
-        for field in &["assignee", "reporter", "creator"] {
-            let user = &fields[*field];
-            let id = user["key"].as_str().or_else(|| user["accountId"].as_str());
-            if let Some(id) = id {
-                return Some(id.to_string());
-            }
+        let user = &fields[f];
+        let id = user["key"].as_str();
+        if let Some(id) = id {
+            return Some(id.to_string());
         }
 
         None
+    }
+}
+
+impl CurrentUserService for DefaultCurrentUserService {
+    fn fetch_current_account_id(&self) -> Option<String> {
+        self.get_user_id_by("assignee")
+            .or_else(|| self.get_user_id_by("reporter"))
+            .or_else(|| self.get_user_id_by("creator"))
     }
 }
